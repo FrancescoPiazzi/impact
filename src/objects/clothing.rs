fdebcbb714a333bd1d48c396b29c408d18eaa3ba
Fre <francescopiazzi25@gmail.com>
2023-12-29 20:49:12 +0100

    added armor piece

--- src/objects/clothing.rs
+++ src/objects/clothing.rs
@@ -1,7 +1,18 @@
+use std::collections::HashMap;
+
+use crate::objects::object::Object;
+use crate::common::damage::DamageType;
+
 trait Clothing{
     fn protect_element(&self);
 }
 
 trait Armor{
     fn protect_attack(&self);
+}
+
+pub struct ArmorPiece{
+    pub object: Object,
+    pub armor_low: HashMap<DamageType, u32>,
+    pub armor_high: u32,
 }
