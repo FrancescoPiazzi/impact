a0f857b109d918f14d07453f87dcd7e360d909cc
Francesco-02 <francescopiazzi25@gmail.com>
2024-03-11 18:54:30 +0100

    used derive more to add stats and removed raw stats cause I didn't like them

--- src/common/stats.rs
+++ src/common/stats.rs
@@ -1,32 +1,28 @@
+extern crate derive_more;
+
+use derive_more::Add;
+
 use rand::Rng;
-use std::ops::Add;
 
 // stats are used as part of actors descriptions but also to describe buffs and debuffs of some objects
-
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct Stats{
-    pub raw_strength: u32,
     pub strength: StrengthStat,
-    pub raw_perception: u32,
     pub perception: PerceptionStat,
-    pub raw_endurance: u32,
     pub endurance: EnduranceStat,
-    pub raw_charisma: u32,
     pub charisma: CharismaStat,
-    pub raw_intelligence: u32,
     pub intelligence: IntelligenceStat,
-    pub raw_agility: u32,
     pub agility: AgilityStat,
 }
 
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct StrengthStat{
     pub max_carry_weight: u32,
     // base force of a blow, influenced by the weight of the weapon, strength of the actor, and wind up time
     pub base_melee_force: u32,
 }
 
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct PerceptionStat{
     pub vision: f32,
     pub hearing: f32,
@@ -35,7 +31,7 @@ pub struct PerceptionStat{
     pub night_vision_modifier: f32,  // multiplied by vision to get cision in the dark
 }
 
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct EnduranceStat{
     pub stamina: u32,
 
@@ -49,7 +45,7 @@ pub struct EnduranceStat{
     pub health_regen: f32,
 }
 
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct CharismaStat{
     pub intimidation: f32,  
     pub persuasion: f32,    
@@ -57,7 +53,7 @@ pub struct CharismaStat{
     pub performance: f32,   // acting
 }
 
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct IntelligenceStat{
     pub sensorary_memory: f32,
     pub text_memory: f32,
@@ -66,7 +62,7 @@ pub struct IntelligenceStat{
     pub creativity: f32,
 }
 
-#[derive(PartialEq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone, Copy, Debug, Add)]
 pub struct AgilityStat{
     pub max_sprint_speed: u32,
     pub climbing: f32,
@@ -148,50 +144,33 @@ impl AgilityStat{
     }
 }
 
+
 impl Stats{
     pub fn new(
-        raw_strength: u32,
         strength: StrengthStat,
-        raw_perception: u32,
         perception: PerceptionStat,
-        raw_endurance: u32,
         endurance: EnduranceStat,
-        raw_charisma: u32,
         charisma: CharismaStat,
-        raw_intelligence: u32,
         intelligence: IntelligenceStat,
-        raw_agility: u32,
         agility: AgilityStat,
     ) -> Stats{
         Stats{
-            raw_strength: raw_strength,
             strength: strength,
-            raw_perception: raw_perception,
             perception: perception,
-            raw_endurance: raw_endurance,
             endurance: endurance,
-            raw_charisma: raw_charisma,
             charisma: charisma,
-            raw_intelligence: raw_intelligence,
             intelligence: intelligence,
-            raw_agility: raw_agility,
             agility: agility,
         }
     }
 
     pub fn new_zero() -> Stats {
         Stats::new(
-            0,
             StrengthStat::new(0, 0),
-            0,
             PerceptionStat::new(0.0, 0.0, 0.0, 0.0, 0.0),
-            0,
             EnduranceStat::new(0, 0, 0, 0.0),
-            0,
             CharismaStat::new(0.0, 0.0, 0.0, 0.0),
-            0,
             IntelligenceStat::new(0.0, 0.0, 0.0, 0.0, 0.0),
-            0,
             AgilityStat::new(0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
         )
     }
@@ -199,13 +178,11 @@ impl Stats{
     pub fn new_random() -> Stats {
         let mut rng = rand::thread_rng();
 
-        let raw_strength = rng.gen_range(1..=10);
         let strength = StrengthStat::new(
             rng.gen_range(1..=10),
             rng.gen_range(1..=10),
         );
 
-        let raw_perception = rng.gen_range(1..=10);
         let perception = PerceptionStat::new(
             rng.gen_range(0.0..=1.0),
             rng.gen_range(0.0..=1.0),
@@ -214,7 +191,6 @@ impl Stats{
             rng.gen_range(0.0..=1.0),
         );
 
-        let raw_endurance = rng.gen_range(1..=10);
         let endurance = EnduranceStat::new(
             rng.gen_range(1..=10),
             rng.gen_range(1..=10),
@@ -222,7 +198,6 @@ impl Stats{
             rng.gen_range(0.001..=0.005),
         );
 
-        let raw_charisma = rng.gen_range(1..=10);
         let charisma = CharismaStat::new(
             rng.gen_range(0.0..=1.0),
             rng.gen_range(0.0..=1.0),
@@ -230,7 +205,6 @@ impl Stats{
             rng.gen_range(0.0..=1.0),
         );
 
-        let raw_intelligence = rng.gen_range(1..=10);
         let intelligence = IntelligenceStat::new(
             rng.gen_range(0.0..=1.0),
             rng.gen_range(0.0..=1.0),
@@ -239,7 +213,6 @@ impl Stats{
             rng.gen_range(0.0..=1.0),
         );
 
-        let raw_agility = rng.gen_range(1..=10);
         let agility = AgilityStat::new(
             rng.gen_range(1..=10),
             rng.gen_range(0.0..=1.0),
@@ -252,122 +225,12 @@ impl Stats{
         );
 
         Stats::new(
-            raw_strength,
             strength,
-            raw_perception,
             perception,
-            raw_endurance,
             endurance,
-            raw_charisma,
             charisma,
-            raw_intelligence,
             intelligence,
-            raw_agility,
             agility,
         )
-    }
-}
-
-
-impl Add for Stats {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            raw_strength: self.raw_strength + other.raw_strength,
-            strength: self.strength + other.strength,
-            raw_perception: self.raw_perception + other.raw_perception,
-            perception: self.perception + other.perception,
-            raw_endurance: self.raw_endurance + other.raw_endurance,
-            endurance: self.endurance + other.endurance,
-            raw_charisma: self.raw_charisma + other.raw_charisma,
-            charisma: self.charisma + other.charisma,
-            raw_intelligence: self.raw_intelligence + other.raw_intelligence,
-            intelligence: self.intelligence + other.intelligence,
-            raw_agility: self.raw_agility + other.raw_agility,
-            agility: self.agility + other.agility,
-        }
-    }
-}
-
-impl Add for StrengthStat {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            max_carry_weight: self.max_carry_weight + other.max_carry_weight,
-            base_melee_force: self.base_melee_force + other.base_melee_force,
-        }
-    }
-}
-
-impl Add for PerceptionStat {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            vision: self.vision + other.vision,
-            hearing: self.hearing + other.hearing,
-            smell: self.smell + other.smell,
-            taste: self.taste + other.taste,
-            night_vision_modifier: self.night_vision_modifier + other.night_vision_modifier,
-        }
-    }
-}
-
-impl Add for EnduranceStat {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            stamina: self.stamina + other.stamina,
-            base_stamina_used_for_movement: self.base_stamina_used_for_movement + other.base_stamina_used_for_movement,
-            base_stamina_used_for_fighting: self.base_stamina_used_for_fighting + other.base_stamina_used_for_fighting,
-            health_regen: self.health_regen + other.health_regen,
-        }
-    }
-}
-
-impl Add for CharismaStat {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            intimidation: self.intimidation + other.intimidation,
-            persuasion: self.persuasion + other.persuasion,
-            deception: self.deception + other.deception,
-            performance: self.performance + other.performance,
-        }
-    }
-}
-
-impl Add for IntelligenceStat {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            sensorary_memory: self.sensorary_memory + other.sensorary_memory,
-            text_memory: self.text_memory + other.text_memory,
-            learning: self.learning + other.learning,
-            application: self.application + other.application,
-            creativity: self.creativity + other.creativity,
-        }
-    }
-}
-
-impl Add for AgilityStat {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            max_sprint_speed: self.max_sprint_speed + other.max_sprint_speed,
-            climbing: self.climbing + other.climbing,
-            riding: self.riding + other.riding,
-            swimming: self.swimming + other.swimming,
-            parkour: self.parkour + other.parkour,
-            stealth: self.stealth + other.stealth,
-            acrobatics: self.acrobatics + other.acrobatics,
-            pickpocting: self.pickpocting + other.pickpocting,
-        }
     }
 }
